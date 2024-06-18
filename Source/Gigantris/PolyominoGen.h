91a824e4bde99dda2703e466d58f9daf8d37748d
seancrwhite <seancrwhite@gmail.com>
2024-06-18 08:24:35 -0700

    missing files

---
+++ Source/Gigantris/PolyominoGen.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PolyominoGen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GIGANTRIS_API UPolyominoGen : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+private:
+	static TArray<FString> BuildStringArrayFromInts(int pieceSize, int** tArray);
+public:
+	UFUNCTION(BlueprintCallable, DisplayName = "Calculate String Array", Category = "PolyominoGen")
+	static TArray<FString> CalcStringArray(const int pieceSize);
+};
